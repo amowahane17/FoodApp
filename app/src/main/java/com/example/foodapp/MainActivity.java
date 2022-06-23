dea19e48d70980fd8470f00c384c164df2094bac
Amol Wahane <amolwahane17@gmail.com>
2022-06-23 17:06:18 +0530

    Origin:v1.0.2 -added SignUpActivity -added SignInActivity

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -2,16 +2,45 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login, register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        login = findViewById(R.id.login_btn);
+        register= findViewById(R.id.register_btn);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent_1 = new Intent(MainActivity.this, SignInActivity.class );
+
+                startActivity(intent_1);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent_2 = new Intent(MainActivity.this, SignUpActivity.class );
+
+                startActivity(intent_2);
+            }
+        });
+
+
 
 
     }
