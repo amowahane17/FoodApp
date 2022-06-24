327ecbb7940df12f779ef323ba82d6039a0a255c
Amol Wahane <amolwahane17@gmail.com>
2022-06-24 06:08:47 +0530

    Origin:v1.0.2 -added Want to create from SignInActivity -added Already have account from SignUpActivity -changed button names from Register to Sign up

--- app/src/main/java/com/example/foodapp/SignInActivity.java
+++ app/src/main/java/com/example/foodapp/SignInActivity.java
@@ -2,15 +2,39 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    TextView createAcc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("Sign in");
+
+
+        createAcc = findViewById(R.id.wantToCreateAcc);
+
+
+
+        createAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent= new Intent(SignInActivity.this,SignUpActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/foodapp/SignUpActivity.java
+++ app/src/main/java/com/example/foodapp/SignUpActivity.java
@@ -2,15 +2,35 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    TextView alreadyHaveACC ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("Sign up");
+
+        alreadyHaveACC = findViewById(R.id.alreayHaveAcc);
+
+        alreadyHaveACC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent= new Intent(SignUpActivity.this,SignInActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
